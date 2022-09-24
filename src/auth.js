59d5ea08cf98caea05c7c9f3d76430a20b708c5a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-09-23 17:32:48 -0700

    Fixes for proper authorization.

--- src/auth.js
+++ src/auth.js
@@ -2,18 +2,14 @@ import * as utils from "./utils.js";
 import * as gh from "./github.js";
 
 export async function identifyUser(request) {
-    if (!("Authentication" in request.headers)) {
-        return null;
-    }
-
-    let auth = request.headers["Authentication"];
-    if (!auth.startsWith("Bearer ")) {
+    let auth = request.headers.get("Authorization");
+    if (auth == null || !auth.startsWith("Bearer ")) {
         return null;
     }
 
     let token = auth.slice(7);
 
-    const tokenCache = caches.open("token:cache");
+    const tokenCache = await caches.open("token:cache");
     let check = await tokenCache.match(token);
     if (check) {
         let info = await check.json();
@@ -22,7 +18,7 @@ export async function identifyUser(request) {
         let info = await gh.identifyUser(token);
         let expiry = new Date(Date.now() + 60 * 60 * 1000);
         info.headers["Expires"] = expiry.toISOString();
-        tokenCache.put(token, info.clone()); // need to store a clone.
+        tokenCache.put(info.url, info.clone()); // need to store a clone.
         return (await info.json()).login;
     }
 }
@@ -30,7 +26,7 @@ export async function identifyUser(request) {
 export async function isAllowedUploader(request) {
     let user = await identifyUser(request);
     if (user == null) {
-        return utils.errorResponse("expected a GitHub PAT in the Authorization header", 401);
+        return utils.errorResponse("failed to determine user from the GitHub token", 401);
     }
 
     let allowed = new Set(["LTLA", "lelongs", "jkanche", "PeteHaitch", "vjcitn"]);
