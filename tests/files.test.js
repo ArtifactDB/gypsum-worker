862976f7d088a9b7f126a537e823044d2388c0f4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-12-04 09:40:44 +1100

    Began fleshing out the tests for the endpoint.

--- tests/files.test.js
+++ tests/files.test.js
@@ -2,11 +2,28 @@ import * as f_ from "../src/index.js";
 import * as files from "../src/files.js";
 import * as setup from "./setup.js";
 
-beforeAll(async () => setup.mockProject("test-public", "base"));
+beforeAll(async () => setup.mockPublicProject());
 
 test("version metadata getter works correctly", async () => {
-    let stuff = await BOUND_BUCKET.get("test-public/base/whee.txt.json");
-    console.log(await stuff.json());
-//    let deets = await files.getVersionMetadataOrNull("test-public", "base", []);
-//    console.log(deets);
+    let nb = [];
+    let deets = await files.getVersionMetadataOrNull("test-public", "base", nb);
+    expect(deets).not.toBeNull();
+    expect("upload_time" in deets).toBe(true);
+    expect(nb.length).toBeGreaterThan(0);
+    await Promise.all(nb);
+
+    // Successfully pulls it out of the cache.
+    {
+        let nb2 = [];
+        let deets2 = await files.getVersionMetadataOrNull("test-public", "base", nb2);
+        expect(deets2).toEqual(deets);
+        expect(nb2.length).toBe(0);
+    }
+
+    // Returns null if the project or version doesn't exist.
+    {
+        let nb2 = [];
+        let deets2 = await files.getVersionMetadataOrNull("test-public", "foo", nb2);
+        expect(deets2).toBeNull();
+    }
 });
--- tests/setup.js
+++ tests/setup.js
@@ -1,26 +1,85 @@
 import * as fs from "fs";
+import * as crypto from "crypto";
 
-export async function mockProject(project, version) {
+function computeHash(contents) {
+    if (typeof contents == "string") {
+        let enc = new TextEncoder;
+        contents = enc.encode(contents);
+    }
+    return crypto.createHash('md5').update(contents).digest('hex');
+}
+
+const jsonmeta = {
+    httpMetadata: { contentType: "application/json" }
+};
+
+export async function mockProjectVersion(project, version, files) {
     let promises = [];
     let base = project + "/" + version;
 
-    let basic = {
-        "$schema": "generic_file/v1.json",
-        "generic_file": {
-            "format": "text"
-        }
-    };
+    let all_meta = [];
+    for (const [rpath, contents] of Object.entries(files)) {
+        promises.push(BOUND_BUCKET.put(base + "/" + rpath, contents));
 
-    let jsonmeta = {
+        let meta = { 
+            "$schema": "generic_file/v1.json",
+            "generic_file": {
+                "format": "text"
+            },
+            md5sum: computeHash(contents), 
+            path: rpath 
+        };
+        all_meta.push(meta);
+
+        promises.push(BOUND_BUCKET.put(base + "/" + rpath + ".json", JSON.stringify(meta), jsonmeta));
+    }
+
+    promises.push(BOUND_BUCKET.put("test-public/base/..aggregated", JSON.stringify(all_meta), jsonmeta)); 
+    promises.push(BOUND_BUCKET.put("test-public/base/..revision",
+        JSON.stringify({
+            upload_time: (new Date).toISOString(),
+            index_time: (new Date).toISOString()
+        }),
+        jsonmeta
+    ));
+
+    await Promise.all(promises);
+    return all_meta;
+}
+
+export async function mockPublicProject() {
+    let contents = "";
+    for (var i = 1; i <= 100; i++) {
+        contents += String(i) + "\n";
+    }
+
+    let payload = {
+        "whee.txt": "a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm\nn\no\np\nq\nr\ns\nt\nu\nv\nw\nx\ny\nz\n",
+        "blah.txt": "A\nB\nC\nD\nE\nF\nG\nH\nI\nJ\nK\nL\nM\nN\nO\nP\nQ\nR\nS\nT\nU\nV\nW\nX\nY\nZ\n",
+        "foo/bar.txt": contents
+    };
+    await mockProjectVersion("test-public", "base", payload);
+
+    payload["whee.txt"] = "Aaron Lun had a little lamb.";
+    await mockProjectVersion("test-public", "modified", payload);
+
+    let json_meta = {
         httpMetadata: { contentType: "application/json" }
     };
 
-    {
-        let rpath = "whee.txt"
-        let rpath0  = rpath + ".json";
-        let contents = "A\nB\nC\nD\nE\nF\nG\nH\nI\nJ\nK\nL\nM\nN\nO\nP\nQ\nR\nS\nT\nU\nV\nW\nX\nY\nZ\n";
-        promises.push(BOUND_BUCKET.put(base + "/" + rpath, contents));
-        let meta = { ...basic, md5sum: "xxxxx", path: rpath };
-        promises.push(BOUND_BUCKET.put(base + "/" + rpath + ".json", JSON.stringify(meta), jsonmeta)); 
-    }
+    // Adding project-level sundries.
+    let latest = { version: "modified", index_time: (new Date).toISOString() };
+    BOUND_BUCKET.put("test-public/..latest", JSON.stringify(latest), jsonmeta);
+    BOUND_BUCKET.put("test-public/..latest_all", JSON.stringify(latest), jsonmeta);
+
+    let perms = {
+        scope: "project",
+        read_access: "public",
+        write_access: "owners",
+        owners: ["ArtifactDB-bot"],
+        viewers:[]
+    };
+    await BOUND_BUCKET.put("test-public/..permissions", JSON.stringify(latest), jsonmeta);
+    
+    return null;
 }
